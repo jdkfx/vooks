973a5e4db87f9161a66d358d2d612cabe1833be8
jdkfx <amateurranch@gmail.com>
2021-01-14 07:29:03 +0900

    :recycle: 冗長な部分を削除

--- src/components/WishButton.vue
+++ src/components/WishButton.vue
@@ -3,8 +3,8 @@
     <v-btn color="blue" v-on:click="addWishList()">読みたいリストに追加</v-btn>
     <v-dialog v-model="wishDialog" max-width="300">
       <v-card>
-        <v-card-text v-if="toPropsWishFlag">「{{ wishTitle }}」が読みたい本のリストに追加されました</v-card-text>
-        <v-card-text v-else>「{{ wishTitle }}」は既に読みたい本のリストに追加されています</v-card-text>
+        <v-card-text v-if="toPropsWishFlag">「{{ toPropsTitle }}」が読みたい本のリストに追加されました</v-card-text>
+        <v-card-text v-else>「{{ toPropsTitle }}」は既に読みたい本のリストに追加されています</v-card-text>
       </v-card>
     </v-dialog>
   </v-container>
@@ -22,14 +22,12 @@ export default {
   data() {
     return {
       wishDialog: false,
-      wishTitle: "",
     }
   },
 
   methods: {
     async addWishList() {
       this.$emit('wish-button');
-      this.wishTitle = this.toPropsTitle;
       return this.wishDialog = true;
     },
   },
