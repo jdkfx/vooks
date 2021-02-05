af8e1dab2546f6fb6231b11effd59e52da0038b0
jdkfx <amateurranch@gmail.com>
2021-02-05 23:22:28 +0900

    :sparkles: Firestoreから取得した本のデータを表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,16 +12,16 @@
             <h2>読みたい本のリスト</h2>
           </div>
 
-          <!-- <div v-if="items !== null">
+          <div v-if="items !== null">
             <ul v-for="item in items" v-bind:key="item.id">
-              <img v-bind:src=item.Item.largeImageUrl />
-              <p>タイトル：{{ item.Item.title }}</p>
-              <p>著者：{{ item.Item.author }}</p>
-              <p>{{ item.Item.itemCaption }}</p>
-              <p>ISBN：{{ item.Item.isbn }}</p>
-              <p>出版社：{{ item.Item.publisherName }}</p>
+              <img v-bind:src=item.imageUrl />
+              <p>タイトル：{{ item.title }}</p>
+              <p>著者：{{ item.author }}</p>
+              <p>{{ item.itemCaption }}</p>
+              <p>ISBN：{{ item.isbn }}</p>
+              <p>{{ item.timestamp.toDate() }}に追加</p>
             </ul>
-          </div> -->
+          </div>
 
         </v-col>
       </v-row>
@@ -44,13 +44,14 @@ export default {
 
   data() {
     return {
-      // なんらかのデータを返す
+      items: [],
     }
   },
 
   methods: {
     // CloudFirestoreに格納された情報を表示する
     async showLists() {
+      let self = this;
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
           let colRef = db.collection("users").doc(user.uid).collection("wishLists");
@@ -61,6 +62,7 @@ export default {
               querySnapshot.forEach(function(doc) {
                 console.log("Document data:", doc.id, " => ", doc.data());
               });
+              self.items = querySnapshot.docs.map(doc => doc.data());
             }
           }).catch(function(error) {
             console.log("Error getting document:", error);
